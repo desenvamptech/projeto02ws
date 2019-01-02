cdf0d44fef23674d03afc4ee84e5c7e6257644dd
Andre Politti <desenv@amptech.com.br>
2019-01-02 12:43:46 -0200

    Segundo commit

---
+++ src/main/java/br/com/amptech/projeto02ws/Projeto02wsApplication.java
@@ -0,0 +1,14 @@
+package br.com.amptech.projeto02ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto02wsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto02wsApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/br/com/amptech/projeto02ws/Projeto02wsApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.amptech.projeto02ws;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Projeto02wsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
